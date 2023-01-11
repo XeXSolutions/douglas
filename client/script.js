ae00f1b03409f105d681603f3cb26cc62547b38e
XeXSolutions <xexdouglas@gmail.com>
2023-01-11 13:24:32 +0800

    update the server url again

--- client/script.js
+++ client/script.js
@@ -84,7 +84,7 @@ const handleSubmit = async (e) => {
   // messageDiv.innerHTML = "..."
   loader(messageDiv);
 
-  const response = await fetch("https://douglas-xexsolutions.vercel.app/", {
+  const response = await fetch("https://douglas-ai.onrender.com/", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
